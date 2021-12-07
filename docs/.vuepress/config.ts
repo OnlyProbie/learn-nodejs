d58d226c65b1a1f003d69ff960667a6d179aa803
wangzhan <wangzhanz@didiglobal.com>
2021-12-07 16:05:54 +0800

    improvement: update path

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: 'NodeJS中文教程',
   description: '',
-
+  base: '/learnnodejs/',
   themeConfig: {
     logo: 'https://nodejs.org/static/images/favicons/favicon.ico',
   },
