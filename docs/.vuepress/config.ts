9f3fe2850e3b06f8d4643139f3c8f7e0fce67a36
AlanWang <onlyprobie@gmail.com>
2021-12-07 17:13:08 +0800

    improvement: update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,9 +5,10 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: 'NodeJS中文教程',
   description: '',
-  base: '/<learnnodejs>/',
+  base: '/learnnodejs/',
   themeConfig: {
     logo: 'https://nodejs.org/static/images/favicons/favicon.ico',
     repo: 'https://github.com/OnlyProbie/learn-nodejs',
+    editLink: false,
   },
 })
